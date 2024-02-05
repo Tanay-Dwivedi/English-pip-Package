42e200d253503a0c5ee306f180eda59866825d0f
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-02-05 20:28:08 +0530

    get sentences done

--- enalsis/__init__.py
+++ enalsis/__init__.py
@@ -2,8 +2,19 @@ import spacy
 
 # Extract entities from string
 
+
 def get_entities(text):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
     entities = [(ent.text, ent.label_) for ent in doc.ents]
     return entities
+
+
+# Extract sentences from a string
+
+
+def get_sentences(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    sentences = [sent.text for sent in doc.sents]
+    return sentences
