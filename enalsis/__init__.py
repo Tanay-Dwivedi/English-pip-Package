5c8bdec4d2f7231feed4299f729125512178ea1d
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-02-07 15:32:14 +0530

    Readability Analysis done

--- enalsis/__init__.py
+++ enalsis/__init__.py
@@ -8,6 +8,7 @@ import nltk
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
+import textstat
 
 nltk.download("en_core_web_sm")
 nltk.download("en_core_web_md")
@@ -417,3 +418,62 @@ def text_summarization(text, num_lines):
     summary = " ".join(top_sentences)
 
     return summary
+
+
+# Readability Analysis
+
+
+def calculate_readability_metrics(text):
+    flesch_reading_ease = textstat.flesch_reading_ease(text)
+    flesch_kincaid_grade = textstat.flesch_kincaid_grade(text)
+    smog_index = textstat.smog_index(text)
+    coleman_liau_index = textstat.coleman_liau_index(text)
+    automated_readability_index = textstat.automated_readability_index(text)
+    dale_chall_readability_score = textstat.dale_chall_readability_score(text)
+    difficult_words = textstat.difficult_words(text)
+    linsear_write_formula = textstat.linsear_write_formula(text)
+    gunning_fog = textstat.gunning_fog(text)
+    text_standard = textstat.text_standard(text)
+    fernandez_huerta = textstat.fernandez_huerta(text)
+    szigriszt_pazos = textstat.szigriszt_pazos(text)
+    gutierrez_polini = textstat.gutierrez_polini(text)
+    crawford = textstat.crawford(text)
+    gulpease_index = textstat.gulpease_index(text)
+    osman = textstat.osman(text)
+
+    average_readability = (
+        flesch_reading_ease
+        + flesch_kincaid_grade
+        + smog_index
+        + coleman_liau_index
+        + automated_readability_index
+        + dale_chall_readability_score
+        + linsear_write_formula
+        + gunning_fog
+        + fernandez_huerta
+        + szigriszt_pazos
+        + gutierrez_polini
+        + crawford
+        + gulpease_index
+        + osman
+    ) / 14
+
+    return {
+        "Flesch Reading Ease": flesch_reading_ease,
+        "Flesch-Kincaid Grade Level": flesch_kincaid_grade,
+        "SMOG Index": smog_index,
+        "Coleman-Liau Index": coleman_liau_index,
+        "Automated Readability Index": automated_readability_index,
+        "Dale-Chall Readability Score": dale_chall_readability_score,
+        "Difficult Words": difficult_words,
+        "Linsear Write Formula": linsear_write_formula,
+        "Gunning Fog Index": gunning_fog,
+        "Text Standard": text_standard,
+        "Fernandez Huerta": fernandez_huerta,
+        "Szigriszt Pazos": szigriszt_pazos,
+        "Gutierrez Polini": gutierrez_polini,
+        "Crawford": crawford,
+        "Gulpease Index": gulpease_index,
+        "Osman": osman,
+        "Average Readability": average_readability,
+    }
